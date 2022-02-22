c52ea38a78613b788d5b4612fe8cbbcca6923074
Denis <arboness@gmail.com>
2022-02-21 22:08:00 -0300

    first commit

---
+++ aula-01/main.js
@@ -0,0 +1,17 @@
+const numbers = [9, 15, 24, 32, 70]
+let result = 0
+
+for (let number of numbers){
+
+    result = number + result
+    console.log(`O resultado é: ${result}`)
+   
+}
+
+   
+
+
+
+
+
+
