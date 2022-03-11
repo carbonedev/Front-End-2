8b1e9271f8f68836a66a90da7d8e1e1b3de9d2d7
Denis <arboness@gmail.com>
2022-03-10 22:05:53 -0300

    aula 05 pronta

---
+++ aula-05-mesa/main.js
@@ -0,0 +1,10 @@
+// grupo 3: Alan Menezes Denis Carbone Marina Fachinello Rodrigo da Silva Araújo Thomas Breno Bertelli Mendes; 
+
+let change = document.querySelector('body')
+
+function changeTema() {
+
+   
+    change.classList.toggle('dark')
+
+}
