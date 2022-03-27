a56eccb00640c02fbda84cc5cc49e63ed296ca1b
Denis <arboness@gmail.com>
2022-03-27 14:05:21 -0300

    atualizacao

--- aula-13/script.js
+++ aula-13/script.js
@@ -2,8 +2,27 @@ let nomeReference = document.querySelector('#nome')
 let passReference = document.querySelector('#pass')
 let telReference = document.querySelector('#tel')
 let buttonReference = document.querySelector('#button')
+let userName = ""
+
+
+
+
+buttonReference.addEventListener('keyup', function(event){
+    
+    userName = nomeReference
+    console.log(userName)
+    if (userName == "") {
+        buttonReference.removeAttribute("disabled");
+    }
+  });
 
 buttonReference.addEventListener('click', function(event){
     event.preventDefault()
+    
+    if(nomeReference.value !== ""){
+    
+        buttonReference.removeAttribute('disabled')
+    
+    }
     
 })
