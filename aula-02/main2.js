775d97a05ba16d88aa4c3eeffe7b9d9f4ac5ea94
Denis <arboness@gmail.com>
2022-03-27 14:01:21 -0300

    modified to work

--- aula-02/main2.js
+++ aula-02/main2.js
@@ -1,243 +1,182 @@
-// var botChoice = 0;
-
-// var userChoice = 0;
-
-
-
-// var botWinner = 0;
-
-// const compScore = document.getElementById('comp-score')
-// compScore.innerHTML = botWinner
-
-// var userWinner = 0;
-// const userScore = document.getElementById('user-score')
-// userScore.innerHTML = userWinner
-
-// var result = "";
-
-// const oldResult = document.getElementById('result')
-// oldResult.innerHTML = result
-
-// var winnerGame = "";
-// const oldResult = document.getElementById('result')
-// oldResult.innerHTML = result
-
-
-// function winner(){
-// if (botWinner == 2){
-//     alert('Tente outra vez o Bot ganhou')
-//     botWinner = 0;
-//     userWinner = 0;
-//     winner()
-// }
-// else if (userWinner == 2){
-//     alert('Parabéns você ganhou')
-//     let playAgain = confirm('você quer jogar novamente?')
-//     if (playAgain){
-//         botWinner = 0;
-//         userWinner = 0;
-//         winner()
-//     }
-//     else{
-//         alert('obrigado por jogar')
-//     }
-
-// }
-// else{
- 
-//     playGame()
-    
-// } 
-
-//         function playGame(){
-
-//             botChoice = parseInt(Math.random()*3+1)
-//             switch(userChoice) {
-            
-//             case 1:                  
-
-//                 if (botChoice == 2){
-    
-//                     result = "You Lost!";
-//                     botWinner = ++botWinner
-                    
-//                     }
-//                 else if (botChoice == 3){
-    
-//                     result = "You win!";
-//                     userWinner = ++userWinner    
-                    
-//                     }
-//                 else {
-    
-//                     result = "Draw!";
-                    
-//                     }
-//             break;
-
-//             case 2:
-    
-//                 if (botChoice == 2){
-    
-//                     result = "Draw!";
-                                        
-//                     }
-    
-//                 else if (botChoice == 3){
-    
-//                     result = "You Lost!";
-//                     botWinner = ++botWinner                   
-//                     }
-    
-//                 else {
-    
-//                     result = "You win!";
-//                     userWinner = ++userWinner  
-                     
-//                     }
-//             break;             
-
-//             default:
-//                 if (botChoice == 3){
-    
-//                     result = "Draw!";
-                    
-                    
-//                     }
-                
-//                 else if (botChoice == 1){
-                
-//                     result = "You Lost!";
-//                     botWinner = ++botWinner
-                   
-//                     }
-                
-//                 else{
-                
-//                     result = "You win!";
-//                     userWinner = ++userWinner 
-                            
-//                     }
-//             break; 
-//             }
-//             winner()
-//         }    
-// }
-// winner()
-
+//escolha do usuario
+let userChoice = 0;
 
 //placar usuario
-var initScoreUser = 0;
+let initScoreUser = 0;
 
-var userScore = document.getElementById('user-score')
-userScore.innerText = initScoreUser;
+let userScoreReference = document.getElementById('user-score')
 
 //placar computador
-var initScoreComp = 0;
+let initScoreComp = 0;
 
-var compScore = document.getElementById('comp-score')
-compScore.innerText = initScoreComp;
+let compScoreReference = document.getElementById('comp-score')
 
-//escolha do usuario
-var userChoice = 0;
+//capturando referencias html
 
-//escolha pedra
-function stoneChoice(){
-    return userChoice = 1;
-
-}
+let paperReference = document.querySelector('#paper')
+let rockReference = document.querySelector('#rock')
+let scissorReference = document.querySelector('#scissor')
+let choiceUserReference = document.querySelector('#choiceUser')
+let choiceCompReference = document.querySelector('#choiceComp')
+let playGameReference = document.querySelector('#playGame')
+let resultReference = document.querySelector('#result')
 
 //escolha papel
-function paperChoice(){
-    return userChoice = 2;
 
-}
+paperReference.addEventListener('click', function(){
+    
+    userChoice = 1;
+    choiceUserReference.innerHTML = `<img id="choiceUser" src="img/paper.png" alt=""></img>`
+    
+})
+
+
+
+//escolha pedra
+rockReference.addEventListener('click', function(){
+    
+    userChoice = 2;
+    choiceUserReference.innerHTML = `<img id="choiceUser" src="img/rock.png" alt=""></img>`
+    
+    
+})
+
 
 //escolha tesoura
-function scissorChoice(){
-    return userChoice = 3;
-
-}
-
-
-//resultado
-var initResult = "";
-
-var result = document.getElementById("result")
-result.innerText = initResult
+scissorReference.addEventListener('click', function(){
+    
+    userChoice = 3;
+    choiceUserReference.innerHTML = `<img id="choiceUser" src="img/scissor.png" alt=""></img>`
+    
+    
+})
 
 
 //o Jogo
-        function playGame(){
 
-            botChoice = parseInt(Math.random()*3+1)
-            switch(userChoice) {
+playGameReference.addEventListener('click', function(){
+    botChoice = parseInt(Math.random()*3+1)
+    switch(userChoice) {
+        
+        case 1:                  
+        
+        if (botChoice == 2){
             
-            case 1:                  
-
-                if (botChoice == 2){
-    
-                    result = "You Lost!";
-                    initScoreComp = ++initScoreComp
+            choiceCompReference.innerHTML = `<img id="choiceComp" src="img/rock.png" alt=""></img>`
+            
+            resultReference.innerHTML = `<div id="result">
+                <p>Paper wraps Rock. You win! </p>
+                </div>`
+                
+                initScoreUser = (initScoreUser)+1
+                
+                
+            }
+            else if (botChoice == 3){
+                
+                choiceCompReference.innerHTML = `<img id="choiceComp" src="img/scissor.png" alt=""></img>`
                     
-                    }
-                else if (botChoice == 3){
-    
-                    result = "You win!";
-                    initScoreUser = ++initScoreUser    
-                    
-                    }
+                resultReference.innerHTML = `<div id="result">
+                <p>Scissors cut Paper. You lose! </p>
+                </div>`
+                
+                initScoreComp = (initScoreComp)+1  
+                
+            }
                 else {
-    
-                    result = "Draw!";
                     
-                    }
-            break;
-
-            case 2:
-    
-                if (botChoice == 2){
-    
-                    result = "Draw!";
-                                        
-                    }
-    
+                    choiceCompReference.innerHTML = `<img id="choiceComp" src="img/paper.png" alt=""></img>`
+                    
+                    resultReference.innerHTML = `<div id="result">
+                    <p>Draw! </p>
+                    </div>`
+                    
+                }
+                break;
+                
+                case 2:
+                    
+                    if (botChoice == 2){
+                        
+                        choiceCompReference.innerHTML = `<img id="choiceComp" src="img/rock.png" alt=""></img>`
+                        
+                        resultReference.innerHTML = `<div id="result">
+                    <p>Draw! </p>
+                    </div>`
+                    
+                }
+                
                 else if (botChoice == 3){
-    
-                    result = "You Lost!";
-                    initScoreComp = ++initScoreComp                
-                    }
-    
+                    
+                    choiceCompReference.innerHTML = `<img id="choiceComp" src="img/scissor.png" alt=""></img>`
+                    
+                    resultReference.innerHTML = `<div id="result">
+                    <p>Rock blunts Scissors. You win! </p>
+                    </div>`
+                    
+                    initScoreUser = (initScoreUser)+1
+                    
+                }
+                
                 else {
-    
-                    result = "You win!";
-                    initScoreUser = ++initScoreUser    
-                     
-                    }
-            break;             
-
-            default:
-                if (botChoice == 3){
-    
-                    result = "Draw!";
                     
+                    choiceCompReference.innerHTML = `<img id="choiceComp" src="img/paper.png" alt=""></img>`
                     
+                    resultReference.innerHTML = `<div id="result">
+                    <p>Paper wraps Rock. You lose! </p>
+                    </div>`
+                    
+                    initScoreComp = (initScoreComp)+1
+                    
+                }
+                break;             
+                
+                default:
+                    if (botChoice == 3){
+                        
+                        choiceCompReference.innerHTML = `<img id="choiceComp" src="img/scissor.png" alt=""></img>`
+                        
+                        resultReference.innerHTML = `<div id="result">
+                        <p>Draw! </p>
+                        </div>`
+                        
+                        
                     }
+                    
+                    else if (botChoice == 1){
+                        
+                        choiceCompReference.innerHTML = `<img id="choiceComp" src="img/paper.png" alt=""></img>`
+                        
+                        resultReference.innerHTML = `<div id="result">
+                        <p>Scissors cut Paper. You win! </p>
+                    </div>`
+                    
+                    initScoreUser = (initScoreUser)+1
+                    
+                }
                 
-                else if (botChoice == 1){
-                
-                    result = "You Lost!";
-                    initScoreComp = ++initScoreComp   
-                   
-                    }
-                
-                else{
-                
-                    result = "You win!";
-                    initScoreUser = ++initScoreUser   
-                            
+                    else{
+                        
+                        choiceCompReference.innerHTML = `<img id="choiceComp" src="img/rock.png" alt=""></img>`
+                        
+                        resultReference.innerHTML = `<div id="result">
+                        <p>Rock blunts Scissors. You lose! </p>
+                        </div>`
+                        
+                        initScoreComp = (initScoreComp)+1
+                        
+                        
                     }
             break; 
-            }
-            winner()
-        }   
+        }
+    })
+        
+    userScoreReference.innerHTML = `<div id="user-score">${initScoreUser}</div>`;
+    compScoreReference.innerHTML = `<div id="user-score">${initScoreComp}</div>`;
+        
+    
+        console.log(initScoreComp)
+        console.log(initScoreUser)
+        
+        
+
